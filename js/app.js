313d3ba6ac19427e71ff897eb158389059926d31
bovojon <bovojon@gmail.com>
2016-06-13 21:18:47 -0400

    Remove mutants from the page when they're deleted

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
 $(document).on('click', 'a.delete', function(ev){
   var li = $(ev.currentTarget).closest('li');
   var id = li.data('li'); // Return the value at the named data store for the first element in the jQuery collection
-  deleteMutant(id);
+  deleteMutant(id, li);
 });
 
 // Only get ajax after everything has loaded
