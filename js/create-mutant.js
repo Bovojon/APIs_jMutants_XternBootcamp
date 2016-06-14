9c52b420aed122562cb0441896d28d99fa0f6d52
bovojon <bovojon@gmail.com>
2016-06-13 21:03:52 -0400

    Separate add-mutants into new file

---
+++ js/create-mutant.js
@@ -0,0 +1,17 @@
+function processMutants(mutants){
+  $.each(mutants, function(i, mutant){
+    addMutant(mutant);
+  });
+}
+
+function addMutant(mutant){
+  var li = $('li.template') // Save a selection of an element by class name to a variable li
+    .clone()
+    .removeClass('template') // Remove the class template from each li
+    .attr('data-id', mutant.id); // Set the attribute data-id to its value mutant.id
+
+  li.find('.mutant-name') // with .find() get the descendants of each elemen in the current set of matched elements of li, filtered by a class selector
+    .html(mutant.mutant_name); //set html to that value
+
+$('#mutantList').append(li);
+}
