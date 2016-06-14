cd26c54f4a34ad8b37e3bfaabfada8d44ec45261
bovojon <bovojon@gmail.com>
2016-06-13 20:44:42 -0400

    Include the mutant ID as a data attribute

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
 
-function processMutants(ajaxResults){
-  $.each(ajaxResults, function(i, mutant){
+function processMutants(mutants){
+  $.each(mutants, function(i, mutant){
     addMutant(mutant);
   });
 }
 
 function addMutant(mutant){
-  $('#mutantList').append('<li>' + mutant.mutant_name + '</li>');
+  $('#mutantList').append('<li data-id="'+ mutant.id + '">' + mutant.mutant_name + '</li>'); // target ul with id #mutantList and append list item with mutant.id
 }
 
 $.get({
