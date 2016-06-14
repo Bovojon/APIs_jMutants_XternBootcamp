d62de61a15776caf1077ab9a68a3ad9b00b1344c
bovojon <bovojon@gmail.com>
2016-06-13 21:15:16 -0400

    Make the delete link work, but not display the changes on the page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
 
+$(document).on('click', 'a.delete', function(ev){
+  var li = $(ev.currentTarget).closest('li');
+  var id = li.data('li'); // Return the value at the named data store for the first element in the jQuery collection
+  deleteMutant(id);
+});
 
 // Only get ajax after everything has loaded
 $(function(){
